7faf5fcba08cb0e9c3e043837e33ef59e1e050a2
SirajAbdul <60885064+SirajAbdul@users.noreply.github.com>
2021-09-02 10:36:56 +0530

    WEB API

---
+++ ParticipentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEBAPI.Models;
+
+namespace WEBAPI.Controllers
+{
+    public class ParticipentController : ApiController
+    {
+        [HttpPost]
+        [Route("api/InsertParticipent")]
+        public Participent Insert(Participent model)
+
+        {
+            using (DBModel db= new DBModel())
+            {
+                db.Participents.Add(model);
+                db.SaveChanges();
+                return model;
+            }
+
+        }
+        [HttpPost]
+        [Route("api/UpdateOutput")]
+        public void UpdateOutput (Participent model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Entry(model).State=System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+        }
+
+    }
+}
